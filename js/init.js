3765e1b5db29de7d28889f5c4a9026f17d734c59
jmrpadrino@gmail.com <jmrpadrino@gmail.com>
2020-04-01 17:46:07 -0500

    Vista perfil del beneficiario

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
 		$('.sidenav').sidenav();
 		$('.modal').modal();
 		$(".dropdown-trigger").dropdown();
+		$('.tabs').tabs();
 
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
